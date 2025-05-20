71ffc6e78567b7fe1a5f583dd9adc257a4db0004
merzenario <estebanmolina159@gmail.com>
2025-05-19 23:02:44 -0500

    actualizacion de cajero 1.905

---
+++ src/main/java/com/bancocpm/atm/controller/AdminController.java
@@ -0,0 +1,73 @@
+package com.bancocpm.atm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bancocpm.atm.entity.Cliente;
+import com.bancocpm.atm.entity.Cuenta;
+import com.bancocpm.atm.entity.TipoCuenta;
+import com.bancocpm.atm.services.ClienteService;
+import com.bancocpm.atm.services.CuentaService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final ClienteService clienteService;
+    private final CuentaService cuentaService;
+
+    @GetMapping
+    public String adminHome() {
+        return "admin/index";
+    }
+
+    @GetMapping("/crear-cliente")
+    public String mostrarFormularioCliente(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "admin/crear-cliente";
+    }
+
+    @PostMapping("/crear-cliente")
+    public String crearCliente(@ModelAttribute Cliente cliente) {
+        clienteService.crearCliente(cliente);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/crear-cuenta")
+    public String mostrarFormularioCuenta(Model model) {
+        model.addAttribute("cuenta", new Cuenta());
+        return "admin/crear-cuenta";
+    }
+
+    @PostMapping("/crear-cuenta")
+    public String crearCuenta(@RequestParam String identificacion,
+            @RequestParam String numero,
+            @RequestParam TipoCuenta tipo,
+            @RequestParam double saldo) {
+        Cliente cliente = clienteService.buscarPorIdentificacion(identificacion).orElseThrow();
+        cuentaService.crearCuenta(cliente, numero, tipo, saldo);
+
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/desbloquear")
+    public String mostrarDesbloqueo() {
+        return "admin/desbloquear";
+    }
+
+    @PostMapping("/desbloquear")
+    public String desbloquearCuenta(@RequestParam String identificacion,
+            @RequestParam String nuevoPin) {
+        clienteService.desbloquearCliente(identificacion, nuevoPin);
+        return "redirect:/admin";
+    }
+
+}
---
+++ src/test/java/com/bancocpm/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancocpm.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
